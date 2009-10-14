eaeb4431d3336aada17d086560796e0783fb38c8
jpm <jpm@cs.uu.nl>
2009-10-14 12:28:12 +0000

    Add generic builders

--- Data/Generics.hs
+++ Data/Generics.hs
@@ -28,6 +28,7 @@ module Data.Generics (
   module Data.Generics.Schemes,   -- traversal schemes (everywhere etc.)
   module Data.Generics.Text,      -- generic read and show
   module Data.Generics.Twins,     -- twin traversal, e.g., generic eq
+  module Data.Generics.Builders,  -- term builders
 
 #ifdef __GLASGOW_HASKELL__
 #ifndef __HADDOCK__
@@ -55,3 +56,4 @@ import Data.Generics.Aliases
 import Data.Generics.Schemes
 import Data.Generics.Text
 import Data.Generics.Twins
+import Data.Generics.Builders
