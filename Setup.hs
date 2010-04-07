24a29f4d47710dd2e71101cbc189aba9a3f246f0
jpm <jpm@cs.uu.nl>
2010-04-07 11:05:18 +0200

    Integrate the testsuite with cabal

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Distribution.Simple
+import Distribution.Simple ( defaultMainWithHooks, simpleUserHooks
+                           , UserHooks(runTests))
+import System.Cmd (system)
 
 main :: IO ()
-main = defaultMain
+main = defaultMainWithHooks hooks
+  where hooks = simpleUserHooks { runTests = runTests' }
+
+-- Runs the testsuite
+runTests' _ _ _ _ = system cmd >> return ()
+  where testdir = "tests"
+        testcmd = "runhaskell ./Main.hs"
+        cmd = "cd " ++ testdir ++ " && " ++ testcmd
