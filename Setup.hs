b09324bc2d33a230e8e50e6c0e1be41806227dc4
jpm <jpm@cs.uu.nl>
2010-04-07 09:13:25 +0000

    Use runCommand instead of system

--- Setup.hs
+++ Setup.hs
@@ -2,14 +2,14 @@ module Main (main) where
 
 import Distribution.Simple ( defaultMainWithHooks, simpleUserHooks
                            , UserHooks(runTests))
-import System.Cmd (system)
+import System.Process (runCommand, waitForProcess)
 
 main :: IO ()
 main = defaultMainWithHooks hooks
   where hooks = simpleUserHooks { runTests = runTests' }
 
 -- Runs the testsuite
-runTests' _ _ _ _ = system cmd >> return ()
+runTests' _ _ _ _ = runCommand cmd >>= waitForProcess >> return ()
   where testdir = "tests"
         testcmd = "runhaskell ./Main.hs"
         cmd = "cd " ++ testdir ++ " && " ++ testcmd
